12aa5fe6543dbd32c220486a28b4b4237979812e
AneelaTasneem <aneelatasneem90@gmail.com>
2025-05-29 14:39:17 +0500

    loops'

---
+++ while-loop.js
@@ -0,0 +1,154 @@
+/*1: Javvascript program to print all natural numbers from 1 to n*/
+
+// let n = 10;
+// let i = 1;
+
+// while (i <= n) {
+//   console.log(i);
+
+//   ++i;
+// }
+
+/*2: Javascript program to print natural numbers in reverse from n to 1*/
+
+// let n1 = 1;
+// let a = 10;
+
+// while (a >= n1) {
+//   console.log(a);
+
+//   --a;
+// }
+
+/*3: Javascript program to print alphabets from a to z */
+
+// let alph1 = 97;
+// let alph2 = 122;
+
+// while (alph1 <= alph2) {
+//   console.log(String.fromCharCode(alph1));
+//   ++alph1;
+// }
+
+/* 4:Javascript program to print all even numbers from 1 to n*/
+
+// let num = 1;
+
+// while (num <= 100) {
+//   if (num % 2 == 0) {
+//     console.log(num);
+//   }
+//   ++num;
+// }
+
+/* 5: Javascript  program to print all odd numbers from 1 to n*/
+
+// let num1 = 1;
+// let num2 = 100;
+
+// while (num1 <= num2) {
+//   if (num1 % 2 == 1) {
+//     console.log(num1);
+//   }
+
+//   ++num1;
+// }
+
+/* 6: Javascript program to find sum of natural numbers from 1 to n*/
+
+// let i = 1;
+// let n = 5;
+// let sum = 0;
+
+// while (i <= n) {
+//   sum = sum + i;
+//   ++i;
+// }
+
+// console.log(sum);
+
+/* 7: Javascript program to find sum of even numbers from 1 to n */
+
+// let i = 1;
+// let n = 20;
+// let sum = 0;
+
+// while (i <= n) {
+//   if (i % 2 == 0) {
+//     sum = sum + i;
+//   }
+//   ++i;
+// }
+
+// console.log(sum);
+
+/* 9: Javascript program to find sum of odd numbers from 1 to n */
+
+// let i = 1;
+// let n = 15;
+// let sum = 0;
+
+// while (i <= n) {
+//   if (i % 2 == 1) {
+//     sum = sum + i;
+//   }
+//   ++i;
+// }
+// console.log(sum);
+
+/* 10: javascript program to print multiplication table of a given number*/
+
+// let i = 1;
+// let n = 4;
+
+// while (i <= 10) {
+//   let multiply = n * i;
+//   console.log(`${n} * ${i} =${multiply}`);
+//   ++i;
+// }
+
+/* 11: Javascript  program to count number of digits in an integer*/ 
+
+// let num = 23579940;
+// let count = 0;
+
+// while (num !== 0) {
+//   num = Math.floor(num / 10);
+//   count++;
+// }
+// console.log(count);
+
+ /* 12: Javascript program to find first and last digit of any number*/ 
+
+// let num = 2947453433;
+// let firstDigit = num;
+
+// let lastDigit = num % 10;
+
+// while (firstDigit >= 10) {
+//   firstDigit = Math.floor(firstDigit / 10);
+// }
+// console.log(firstDigit);
+// console.log(lastDigit);
+
+/* 13: Javascript program to find sum of first and last digit of any number*/ 
+
+// let number = 33873497387877;
+// let firstDigit = number;
+
+// let lastDigit = number % 10;
+
+// while (firstDigit >= 10){
+//     firstDigit = Math.floor( firstDigit/ 10 )
+// }
+
+// let sum = firstDigit + lastDigit;
+// console.log(sum);
+
+/* 14:  Javascript program to swap first and last digit of a number */ 
+
+
+
+
+
+
